4075cb796f010f29859856be0873e1b6d1f53453
Felipe Ferreira <felipeferreira.89@hotmail.com>
2023-03-17 21:29:54 -0300

    deixando o ambiente mais limpo

--- main.js
+++ main.js
@@ -8,7 +8,6 @@ function adicionarContato() {
 const nome = document.querySelector('#nome-contato').value;
 const telefone = document.querySelector('#telefone-contato').value;
 
-  // Verifica se o contato já existe
 const contatoExistente = contatos.find(contato => contato.nome === nome);
 const telefoneExistente = contatos.find(contato => contato.telefone === telefone);
 
@@ -27,11 +26,9 @@ if (telefoneExistente) {
     return;
 }
 
-  // Cria um novo objeto de contato
 const contato = { nome, telefone };
 contatos.push(contato);
 
-  // Cria uma nova linha na tabela para exibir o novo contato
 const newRow = document.createElement('tr');
 const nomeCell = document.createElement('td');
 const telefoneCell = document.createElement('td');
@@ -41,9 +38,7 @@ newRow.appendChild(nomeCell);
 newRow.appendChild(telefoneCell);
 tbody.appendChild(newRow);
 
-  // Atualiza o total de contatos
 totalContatos.textContent = contatos.length;
 
-  // Limpa os campos do formulário
 form.reset();
 }
